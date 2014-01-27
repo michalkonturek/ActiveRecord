0bfe46e4dc87b17fcddf3a9fe4f6ddf51778e054
Michal Konturek <michal.konturek@gmail.com>
2014-01-27 08:16:09 +0000

    Added spec.

---
+++ Source/ActiveRecord.m
@@ -0,0 +1,13 @@
+//
+//  ActiveRecord.m
+//  ActiveRecord
+//
+//  Created by Michal Konturek on 27/01/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import "ActiveRecord.h"
+
+@implementation ActiveRecord
+
+@end
