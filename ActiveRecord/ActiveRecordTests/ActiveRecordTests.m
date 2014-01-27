54e4a1be66795e65aa318eb451dd6cf2e59e6ac3
Michal Konturek <michal.konturek@gmail.com>
2014-01-27 08:11:29 +0000

    Initial project.

---
+++ ActiveRecord/ActiveRecordTests/ActiveRecordTests.m
@@ -0,0 +1,34 @@
+//
+//  ActiveRecordTests.m
+//  ActiveRecordTests
+//
+//  Created by Michal Konturek on 27/01/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ActiveRecordTests : XCTestCase
+
+@end
+
+@implementation ActiveRecordTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
