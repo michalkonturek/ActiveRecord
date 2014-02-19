70c8e3bbd2abb6eb51c4ef8603488b6e2abc0f6d
Michal Konturek <michal.konturek@gmail.com>
2014-02-19 20:44:37 +0000

    Added .

---
+++ Source/ActiveRecord+NSManagedObjectContext.h
@@ -0,0 +1,13 @@
+//
+//  ActiveRecord+NSManagedObjectContext.h
+//  ActiveRecord
+//
+//  Created by Michal Konturek on 19/02/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import "ActiveRecord.h"
+
+@interface ActiveRecord (NSManagedObjectContext)
+
+@end
---
+++ Source/ActiveRecord+NSManagedObjectContext.m
@@ -0,0 +1,13 @@
+//
+//  ActiveRecord+NSManagedObjectContext.m
+//  ActiveRecord
+//
+//  Created by Michal Konturek on 19/02/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import "ActiveRecord+NSManagedObjectContext.h"
+
+@implementation ActiveRecord (NSManagedObjectContext)
+
+@end
