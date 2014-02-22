93f6e9ad509081394dd6c0dfe71cf25741343ae8
Michal Konturek <michal.konturek@gmail.com>
2014-02-22 20:28:37 +0000

    Added Kiwi test.

---
+++ ActiveRecord/ActiveRecordTests/Factory.m
@@ -0,0 +1,13 @@
+//
+//  Factory.m
+//  ActiveRecord
+//
+//  Created by Michal Konturek on 22/02/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import "Factory.h"
+
+@implementation Factory
+
+@end
---
+++ ActiveRecord/ActiveRecordTests/NSManagedObjectSpec.m
@@ -0,0 +1,34 @@
+//
+//  NSManagedObjectSpec.m
+//  ActiveRecord
+//
+//  Created by Michal Konturek on 22/02/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSManagedObjectSpec : XCTestCase
+
+@end
+
+@implementation NSManagedObjectSpec
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
