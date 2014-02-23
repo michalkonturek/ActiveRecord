ac9f5601f62c83e36a20eb410d203922bed0d997
Michal Konturek <michal.konturek@gmail.com>
2014-02-23 15:51:09 +0000

    Rewrote tests.

--- ActiveRecord/ActiveRecordTests/NSManagedObjectSpec.m
+++ ActiveRecord/ActiveRecordTests/NSManagedObjectSpec.m
@@ -14,68 +14,59 @@
 
 SPEC_BEGIN(NSManagedObjectSpec)
 
-describe(@"NSManagedObjectSpec", ^{
-    
-    NSInteger count = 20;
-    
-    beforeEach(^{
-        [Student deleteAll];
-    });
+describe(@"NSManagedObject+AR", ^{
 
-    context(@"Create", ^{
+    
+    describe(@"+create", ^{
         
-        it(@"should create object", ^{
-            Student *student = [Student createWithID:@100];
-            student.firstName = @"John";
-            student.lastName = @"Doe";
-            student.age = @21;
+        __block Student *sut;
+        
+        beforeEach(^{
+            [Student deleteAll];
+            [Student commit];
+            sut = [Student create];
+        });
+
+        specify(^{
+            [[sut shouldNot] beNil];
+        });
+        
+        specify(^{
+            [[sut should] beMemberOfClass:[Student class]];
+        });
+        
+        it(@"it should not commit changes", ^{
+            [[[Student objects] should] haveCountOf:1];
+            [Student rollback];
+            [[[Student objects] should] haveCountOf:0];
+        });
+        
+        context(@"withID", ^{
             
-            id result = [Student objects][0];
-            [[result should] beMemberOfClass:[Student class]];
-            [[[result firstName] should] equal:@"John"];
-            [[[result lastName] should] equal:@"Doe"];
-            [[[result age] should] equal:theValue(21)];
+            __block NSNumber *uid = @1;
+            
+            beforeEach(^{
+                [Student deleteAll];
+                sut = [Student createWithID:uid];
+            });
+            
+            it(@"should has ID", ^{
+                [[sut.uid should] equal:uid];
+            });
+            
+            it(@"should not create duplicate object with the same ID", ^{
+                id other = [Student createWithID:uid];
+                [[other should] equal:sut];
+            });
         });
     });
     
-    context(@"Read", ^{
+    describe(@"+deleteAll", ^{
+        
+        NSInteger count = 20;
         
         beforeEach(^{
-            [Factory createStudents:count];
-        });
-        
-        it(@"should read ALL objects", ^{
-            [[[Student objects] should] haveCountOf:count];
-        });
-        
-        it(@"should read only objects that match predicate", ^{
-            id predicate = [NSPredicate predicateWithFormat:@"age >= 30"];
-            [[[Student objectsWithPredicate:predicate] should] haveCountOf:10];
-        });
-        
-        it(@"should read only a specified object", ^{
-            id item = [Student objectWithID:@1];
-            [[[item uid] should] equal:@1];
-            [[[item firstName] should] equal:@"firstName1"];
-            [[[item lastName] should] equal:@"lastName1"];
-            [[[item age] should] equal:@21];
-        });
-        
-        it(@"should return only ", ^{
-            
-        });
-    });
-    
-//    context(@"Update", ^{
-//       
-//        it(@"", ^{
-//            
-//        });
-//    });
-    
-    context(@"Delete", ^{
-        
-        beforeEach(^{
+            [Student deleteAll];
             [Factory createStudents:count];
         });
         
@@ -85,15 +76,104 @@ describe(@"NSManagedObjectSpec", ^{
             [[[Student objects] should] haveCountOf:0];
         });
         
-        it(@"it should delete specific object", ^{
-            [[[Student objects] should] haveCountOf:count];
-
+    });
+    
+    describe(@"-delete", ^{
+        
+        it(@"it should delete specified objects", ^{
+            [Student createWithID:@1];
+            [Student createWithID:@2];
+            [[[Student objects] should] haveCountOf:2];
+            
             [[Student objectWithID:@1] delete];
-
+            
             [[[Student objectWithID:@1] should] beNil];
-            [[[Student objects] should] haveCountOf:(count - 1)];
+            [[[Student objects] should] haveCountOf:1];
         });
     });
+    
+
+    
+    
+    
+//    NSInteger count = 20;
+//    
+//    beforeEach(^{
+//        [Student deleteAll];
+//    });
+//
+//    context(@"Create", ^{
+//        
+//        it(@"should create object", ^{
+//            Student *student = [Student createWithID:@100];
+//            student.firstName = @"John";
+//            student.lastName = @"Doe";
+//            student.age = @21;
+//            
+//            id result = [Student objects][0];
+//            [[result should] beMemberOfClass:[Student class]];
+//            [[[result firstName] should] equal:@"John"];
+//            [[[result lastName] should] equal:@"Doe"];
+//            [[[result age] should] equal:theValue(21)];
+//        });
+//    });
+//    
+//    context(@"Read", ^{
+//        
+//        beforeEach(^{
+//            [Factory createStudents:count];
+//        });
+//        
+//        it(@"should read ALL objects", ^{
+//            [[[Student objects] should] haveCountOf:count];
+//        });
+//        
+//        it(@"should read only objects that match predicate", ^{
+//            id predicate = [NSPredicate predicateWithFormat:@"age >= 30"];
+//            [[[Student objectsWithPredicate:predicate] should] haveCountOf:10];
+//        });
+//        
+//        it(@"should read only a specified object", ^{
+//            id item = [Student objectWithID:@1];
+//            [[[item uid] should] equal:@1];
+//            [[[item firstName] should] equal:@"firstName1"];
+//            [[[item lastName] should] equal:@"lastName1"];
+//            [[[item age] should] equal:@21];
+//        });
+//        
+//        it(@"should return only ", ^{
+//            
+//        });
+//    });
+//    
+////    context(@"Update", ^{
+////       
+////        it(@"", ^{
+////            
+////        });
+////    });
+//    
+//    context(@"Delete", ^{
+//        
+//        beforeEach(^{
+//            [Factory createStudents:count];
+//        });
+//        
+//        it(@"it should delete all objects", ^{
+//            [[[Student objects] should] haveCountOf:count];
+//            [Student deleteAll];
+//            [[[Student objects] should] haveCountOf:0];
+//        });
+//        
+//        it(@"it should delete specific object", ^{
+//            [[[Student objects] should] haveCountOf:count];
+//
+//            [[Student objectWithID:@1] delete];
+//
+//            [[[Student objectWithID:@1] should] beNil];
+//            [[[Student objects] should] haveCountOf:(count - 1)];
+//        });
+//    });
     
 });
 
