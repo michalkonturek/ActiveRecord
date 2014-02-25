67788a649320321d5f197cf8108aff12d5078dd8
Michal Konturek <michal.konturek@gmail.com>
2014-02-25 20:56:40 +0000

    .

---
+++ ActiveRecord/ActiveRecordTests/NSSortDescriptor+AR_Spec.m
@@ -0,0 +1,48 @@
+//
+//  NSSortDescriptor+AR_Spec.m
+//  ActiveRecord
+//
+//  Created by Michal Konturek on 25/02/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+
+#import "ActiveRecord.h"
+#import "Domain.h"
+#import "Factory.h"
+
+SPEC_BEGIN(NSSortDescriptor_AR_Spec)
+
+describe(@"NSSortDescriptor_AR", ^{
+    
+    describe(@"+createFrom", ^{
+        
+//        it(@"should create from NSPredicate", ^{
+//            id input = [NSPredicate predicateWithFormat:@"name == %@", @"Adam"];
+//            id expected = @"name == \"Adam\"";
+//            id result = [NSPredicate createFrom:input];
+//            [[result should] beKindOfClass:[NSPredicate class]];
+//            [[[result description] should] equal:expected];
+//        });
+//        
+//        it(@"should create from NSString", ^{
+//            id input = @"name == 'Adam'";
+//            id expected = @"name == \"Adam\"";
+//            id result = [NSPredicate createFrom:input];
+//            [[result should] beKindOfClass:[NSPredicate class]];
+//            [[[result description] should] equal:expected];
+//        });
+//        
+//        it(@"should create from NSDictionary", ^{
+//            id input = @{@"name": @"Adam"};
+//            id expected = @"name == \"Adam\"";
+//            id result = [NSPredicate createFrom:input];
+//            [[result should] beKindOfClass:[NSPredicate class]];
+//            [[[result description] should] equal:expected];
+//        });
+    });
+    
+});
+
+SPEC_END
---
+++ Source/NSSortDescriptor+AR.m
@@ -0,0 +1,13 @@
+//
+//  NSSortDescriptor+AR.m
+//  ActiveRecord
+//
+//  Created by Michal Konturek on 25/02/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import "NSSortDescriptor+AR.h"
+
+@implementation NSSortDescriptor (AR)
+
+@end
