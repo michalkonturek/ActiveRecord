c9d61f9ace0c18fb1c7945e50a38a728e6e731d8
Michal Konturek <michal.konturek@gmail.com>
2014-04-09 01:17:33 +0100

    Updated specs.

--- ActiveRecord/Pods/Pods-environment.h
+++ ActiveRecord/Pods/Pods-environment.h
@@ -10,11 +10,11 @@
 #define COCOAPODS_POD_AVAILABLE_MKFoundationKit_Block
 #define COCOAPODS_VERSION_MAJOR_MKFoundationKit_Block 1
 #define COCOAPODS_VERSION_MINOR_MKFoundationKit_Block 2
-#define COCOAPODS_VERSION_PATCH_MKFoundationKit_Block 0
+#define COCOAPODS_VERSION_PATCH_MKFoundationKit_Block 2
 
 // RubySugar
 #define COCOAPODS_POD_AVAILABLE_RubySugar
 #define COCOAPODS_VERSION_MAJOR_RubySugar 1
 #define COCOAPODS_VERSION_MINOR_RubySugar 2
-#define COCOAPODS_VERSION_PATCH_RubySugar 1
+#define COCOAPODS_VERSION_PATCH_RubySugar 3
 
